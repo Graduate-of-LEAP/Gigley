bb19891420b5f00b8fb4893ec874649be9c78f6f
Bayra <0523egulnn@bayras-macbok.local>
2024-10-14 12:22:37 +0800

    second

---
+++ fe/src/app/tasker-side/BecomeTasker/page.tsx
@@ -0,0 +1,15 @@
+import { EarnMoney } from '@/components/BecomeTasker-components/EarnMoney';
+import { FlexibleWork } from '@/components/BecomeTasker-components/FlexibleWork';
+import { Footer } from '@/components/assets/Footer';
+import { Header } from '@/components/assets/Header';
+
+export default function () {
+  return (
+    <>
+      <Header />
+      <FlexibleWork />
+      <EarnMoney />
+      <Footer />
+    </>
+  );
+}
