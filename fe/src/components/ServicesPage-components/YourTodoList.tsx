beceacd65f0522713c575b10944d42eb132121d6
24LP6028 24LP6028 <24LP6028@G304-CC0099.local>
2024-10-21 10:17:07 +0800

    17-yourtodolist

--- fe/src/components/ServicesPage-components/YourTodoList.tsx
+++ fe/src/components/ServicesPage-components/YourTodoList.tsx
@@ -1,9 +1,17 @@
 import { Container } from '../assets/Container';
+import Image from 'next/image';
 
 export const YourTodoList = () => {
   return (
-    <Container className="bg-white">
-      <div className="h-10 bg-red-600">YourTodoList</div>
+    <Container className="relative w-screen h-[420px]">
+      <Image
+      src="/picture/Handy.png"
+      alt="Background"
+      layout="fill"
+      objectFit="cover"
+      />
+      <div className="absolute mt-[200px] ml-[300px]">
+        <p className="font-bold text-4xl text-white w-[500px] text-center font">Та хийхээр төлөвлөж байгаа ажилаа бидэнд даатга</p></div>
     </Container>
   );
 };
