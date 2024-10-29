b129934bf722144d847cd1adf3d929a357e87b70
LPD243578 LPD243578 <LPD243578@G301-CC0202.local>
2024-10-29 13:19:27 +0800

    User middleware, getme done

--- be/src/routes/user.router.ts
+++ be/src/routes/user.router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { registerController } from '../controllers/user/register-user.controller';
 import { loginController } from '../controllers/user/login-user.controller';
+import { getMeController } from '../controllers/user/get-me.controller';
 
 const userRouter = Router();
 
-userRouter.post('/register', registerController);
-userRouter.post('/login', loginController);
+userRouter
+  .post('/register', registerController)
+  .post('/login', loginController)
+  .get('/me', getMeController);
 
 export { userRouter };
