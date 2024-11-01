31df657a0ff1bca1fbf4484465874561f372699b
LPD243578 LPD243578 <LPD243578@G301-CC0202.local>
2024-11-01 11:16:06 +0800

    da

--- fe/src/lib/axios/index.ts
+++ fe/src/lib/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiGig = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: 'https://gigley.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
